39894a80a4d395b54046511f75c1210313c0c4b0
umgi <gtdsocial@icloud.com>
2021-09-14 11:51:42 +0300

    add: gulp error notify

--- 02-thrivetalk/gulpfile.js
+++ 02-thrivetalk/gulpfile.js
@@ -1,10 +1,15 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const bs = require('browser-sync').create()
+const plumber = require('gulp-plumber')
+const notify = require('gulp-notify')
 
 const style = () =>
   gulp
     .src('./scss/main.scss')
+    .pipe(
+      plumber({ errorHandler: notify.onError('Error: <%= error.message %>') })
+    )
     .pipe(sass())
     .pipe(gulp.dest('./css'))
     .pipe(bs.stream())
